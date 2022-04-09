0679e3379b156bac13d80ef5e5e1230134a7bbe1
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-04-09 08:15:54 +0530

    Update bigint.js

--- knex-master/test/integration/datatype/bigint.js
+++ knex-master/test/integration/datatype/bigint.js
@@ -29,7 +29,7 @@ module.exports = function (knex) {
       })
       .then(function (row) {
         // triggers request execution
-      })
+      }) 
       .then(function () {  
         return knex(tableName)
           .where('expiry', negativeUnsafeBigint)
